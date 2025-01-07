db072a3697ee9e5d17e929f72c619b4f9bef035c
NullMC <franciscopperfield8@gmail.com>
2025-01-07 20:37:16 +0100

    Finished exercise

--- static/JS/index.js
+++ static/JS/index.js
@@ -1,7 +1,20 @@
-let elements = document.querySelectorAll(".el")
+// Select all elements with the class 'item'
+let elements = document.querySelectorAll(".el");
 
-function handle(el, index){
-    
-}
+// Add a click event listener to each item
+elements.forEach(element => {
+    element.addEventListener('click', function(event) {
+        // Use event.currentTarget to refer to the element that was clicked
+        const clickedElement = event.currentTarget;
 
-elements.forEach(handle)
+        if (clickedElement.classList.contains("green")){
+            clickedElement.classList.remove("green");
+            clickedElement.classList.add("yellow");
+        }
+
+        else if (clickedElement.classList.contains("yellow")){
+            clickedElement.classList.remove("yellow");
+            clickedElement.classList.add("green");
+        }
+    });
+});
